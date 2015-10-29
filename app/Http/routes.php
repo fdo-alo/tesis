e3efb1536c934af7544c0e7900321e094b1b020d
darkenrule <fernandojesus.gonzalezalonso@gmail.com>
2015-10-29 17:56:26 -0600

    Se añadió ruta

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,7 @@ Route::get('/', 'WelcomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
+    'cines' => 'PuntosController',
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
