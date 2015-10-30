a78987b630276f9d9d72809184b9ad97040b9398
darkenrule <fernandojesus.gonzalezalonso@gmail.com>
2015-10-29 20:58:01 -0600

    Peticion

--- app/Http/Controllers/PuntosController.php
+++ app/Http/Controllers/PuntosController.php
@@ -13,14 +13,13 @@ class PuntosController extends Controller {
      * @param Request $request
      */
     public function postIndex(Request $request){
-        if($request->ajax())
-        {
-            $puntos = $request->all();
+
+            return $request->all();
             $lat = $puntos[0];
             $lon = $puntos[1];
             $cines = DB::select("select name, ST_ASTEXT(way) from planet_osm_point where ST_DWithin(way, ST_GeographyFromText('SRID=4326;POINT($lon $lat)'), 3000) and name like '%Cine%'");
             return response()->json($cines,200);
-        }
+        
 
     }
 
