942086f26d4ace3ea2ba228639e380536e9adc09
darkenrule <fernandojesus.gonzalezalonso@gmail.com>
2016-09-19 18:33:39 -0500

    cambio de query

--- app/Http/Controllers/GenerosController.php
+++ app/Http/Controllers/GenerosController.php
@@ -20,8 +20,11 @@ class GenerosController extends Controller {
         $generos = $request->all();
         $cadena = $generos['cadena'];
 
-        $cines = DB::select("select * from (select id, titulo, clasificacion from peliculas where id in (	select pelicula_id from genero_pelicula where genero_id in (select ig_genero2 from similitudes where  $cadena  order by similitud desc  limit 10))Limit 10)as v");
+        /* Hasta aquí funcionaba
+        $cines = DB::select("select * from (select id, titulo, clasificacion from peliculas where id in (	select pelicula_id from genero_pelicula where genero_id in (select id_genero2 from similitudes where  $cadena  order by similitud desc  limit 10))Limit 10)as v");
+*/
 
+        $cines = DB::select("select titulo, clasificacion, genero, similitud from 	peliculas, generos, genero_pelicula, similitudes  where	genero_pelicula.pelicula_id = peliculas.id and 	genero_pelicula.genero_id = generos.id and ($cadena) and id_genero2 = generos.id order by similitud desc limit 10");
 
         return response()->json($cines,200);
 
